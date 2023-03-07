c50867318e238e1c85366352284c31bd69997d3b
tailong.wang@take2.health <tailong.wang@take2.health>
2023-03-07 11:16:30 +0800

    ✅验证theme和媒体查询的可行性

--- gatsby-starter-default/src/pages/page-2a.js
+++ gatsby-starter-default/src/pages/page-2a.js
@@ -3,21 +3,29 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
 import { App } from "my-cra-app"
+import { Box, ThemeProvider, useMediaQuery, useTheme } from "@material-ui/core"
+import themes from "./_lightMuiTheme"
 
-const SecondPageCRA = () => (
-  <Layout>
-    <h1>Hi from the hybrid second page</h1>
-    <p>Welcome to page 2 with CRA component</p>
-    
-    
-    <App />
+function SecondPageCRA() {
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down("xs"))
 
-    
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+  return (
+    <ThemeProvider theme={themes}>
+      <Layout>
+        <h1>Hi from the hybrid second page</h1>
+        <p>Welcome to page 2 with CRA component</p>
+        <App isMobile={matches} />
+        <Box color={matches ? "primary.main" : "secondary.main"}>
+          内部使用在Gatsby
+        </Box>
+
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    </ThemeProvider>
+  )
+}
 
 export const Head = () => <Seo title="Page two with CRA" />
 
--- my-cra-app/src/App.js
+++ my-cra-app/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Box } from '@material-ui/core'
 
-function App() {
+const App = (isMobile) => {
+  const matches = isMobile?.isMobile
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          Learn
         </a>
       </header>
+      <Box color={matches ? 'primary.main' : 'secondary.main'}>
+        测试一下theme my-cra-app
+      </Box>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
