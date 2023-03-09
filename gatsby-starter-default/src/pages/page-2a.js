32e8c001276c79b6be4ed86fa39c538338d2e75c
Lois-Wistkey <lois.xie@wistkey.com>
2023-03-09 14:56:44 +0800

    -test: i18n&router in cra

--- gatsby-starter-default/src/pages/page-2a.js
+++ gatsby-starter-default/src/pages/page-2a.js
@@ -1,25 +1,35 @@
-import * as React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { App } from "my-cra-app"
-import { Box, ThemeProvider, useMediaQuery, useTheme } from "@material-ui/core"
+import {
+  Box,
+  ThemeProvider,
+  useMediaQuery,
+  useTheme,
+  Button,
+} from "@material-ui/core"
 import themes from "./_lightMuiTheme"
 
 function SecondPageCRA() {
   const theme = useTheme()
   const matches = useMediaQuery(theme.breakpoints.down("xs"))
+  const [lang, setlang] = useState("en")
 
   return (
     <ThemeProvider theme={themes}>
       <Layout>
         <h1>Hi from the hybrid second page</h1>
         <p>Welcome to page 2 with CRA component</p>
-        <App isMobile={matches} />
+        <Button onClick={() => setlang("cn")}>切换到中文</Button>
+        <Button onClick={() => setlang("en")}>切换到en</Button>
+        <App isMobile={matches} lang={lang} />
         <Box color={matches ? "primary.main" : "secondary.main"}>
           内部使用在Gatsby
         </Box>
+        <p>Welcome to page 2 with CRA component</p>
 
         <Link to="/">Go back to the homepage</Link>
       </Layout>
--- my-cra-app/src/App.js
+++ my-cra-app/src/App.js
@@ -1,31 +1,120 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { Box } from '@material-ui/core'
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Translation } from "react-i18next";
+import {
+  HashRouter,
+  useLocation,
+  useNavigate,
+  Route,
+  Link,
+  Switch,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
-const App = (isMobile) => {
-  const matches = isMobile?.isMobile
+import "./i18n/i18n";
+import i18n from "i18next";
+
+import { Box } from "@material-ui/core";
+const App = ({ isMobile, lang }) => {
+  const matches = isMobile;
+  useEffect(() => {
+    console.log(lang);
+
+    i18n.changeLanguage(lang);
+  }, [lang]);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <HashRouter>
+        <main>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to={`/about`}>About</Link>
+              </li>
+              <li>
+                <Link
+                  to={{
+                    pathname: "/login",
+                    // state: { from: location },
+                  }}
+                  className="name-load"
+                >
+                  去登录
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          {/* <Switch> */}
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            {/* <Route path="/contact" component={Contact} /> */}
+            {/*404 page*/}
+            <Route render={() => <h1>404: page not found</h1>} />
+            {/* </Switch> */}
+          </Routes>
+        </main>
+      </HashRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn
         </a>
       </header>
-      <Box color={matches ? 'primary.main' : 'secondary.main'}>
+      <Box color={matches ? "primary.main" : "secondary.main"}>
         测试一下theme my-cra-app
       </Box>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const Home = () => (
+  <Translation>
+    {(t) => {
+      return (
+        <div
+          style={{ background: "red", margin: 20, color: "white", width: 200 }}
+        >
+          {t("hello")}
+        </div>
+      );
+    }}
+  </Translation>
+);
+const About = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log("lois check About location::", location);
+  return (
+    <>
+      <h1>About</h1>
+      <button onClick={() => navigate("/login", { state: location })}>
+        go login page
+      </button>
+    </>
+  );
+};
+const Login = () => {
+  const location = useLocation();
+
+  // const from = location.state?.from?.pathname || "/";
+  console.log("lois check login location::---", location);
+  return <h1>Login</h1>;
+};
+
+export default App;
